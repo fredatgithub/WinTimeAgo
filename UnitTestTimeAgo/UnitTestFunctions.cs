23351f4c4e266505875ffb66f78e5b3375e31c2c
fred <domaine75@gmail.com>
2018-07-10 21:01:30 +0200

    Add test project

---
+++ UnitTestTimeAgo/UnitTestFunctions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TimeAgo;
+
+namespace UnitTestTimeAgo
+{
+  [TestClass]
+  public class UnitTestFunctions
+  {
+    [TestMethod]
+    public void TestMethodCreateTimeSentence()
+    {
+      DateTime source = new DateTime(2018, 7, 10);
+      string expected = $"20 hours 58 minutes 49 seconds 272 milliseconds";
+      string result = FormMain.CreateTimeSentence(source);
+      Assert.AreEqual(expected, result);
+    }
+  }
+}
