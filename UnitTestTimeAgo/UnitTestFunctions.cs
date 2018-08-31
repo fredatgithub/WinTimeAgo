c5298bcedc6fe77ecca554334be2f11acf2c48c1
Fred <domaine75@gmail.com>
2018-08-31 18:38:34 +0200

    Add unit test

--- UnitTestTimeAgo/UnitTestFunctions.cs
+++ UnitTestTimeAgo/UnitTestFunctions.cs
@@ -8,10 +8,19 @@ namespace UnitTestTimeAgo
   public class UnitTestFunctions
   {
     [TestMethod]
-    public void TestMethodCreateTimeSentence()
+    public void TestMethodCreateTimeSentence_add_1_minute()
     {
       DateTime source = DateTime.Now.AddMinutes(-1);
       string expected = $"1 minute";
+      string result = FormMain.CreateTimeSentenceUS(source);
+      Assert.IsTrue(result.StartsWith(expected));
+    }
+
+    [TestMethod]
+    public void TestMethodCreateTimeSentence_add_2_minutes()
+    {
+      DateTime source = DateTime.Now.AddMinutes(-2);
+      string expected = $"2 minutes";
       string result = FormMain.CreateTimeSentenceUS(source);
       Assert.IsTrue(result.StartsWith(expected));
     }
