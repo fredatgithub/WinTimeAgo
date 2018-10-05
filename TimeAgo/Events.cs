59a745d010e68ee3e37fb513317c27d862eefe23
Fred <domaine75@gmail.com>
2018-10-05 23:12:18 +0200

    Recreate Events class

---
+++ TimeAgo/Events.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace TimeAgo
+{
+  internal class Events
+  {
+    public List<Event> ListOfEvents { get; set; }
+
+    public Events()
+    {
+      ListOfEvents = new List<Event>();
+    }
+  }
+}
